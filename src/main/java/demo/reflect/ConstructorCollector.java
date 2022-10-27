a4f938fc85813b1f7af22585f981c72a0e3633cd
wujieren <rwj005@outlook.com>
2022-10-27 23:09:53 +0800

    反射：获取/使用  类的构造方法

---
+++ src/main/java/demo/reflect/ConstructorCollector.java
@@ -0,0 +1,55 @@
+package demo.reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @Author rwj
+ * @Date 2022/10/27
+ * @Description
+ *
+ * 通过Class对象可以获取类中的：构造方法；
+ *      1).批量获取构造方法：
+ *          public Constructor[] getConstructors()：所有"公有的"构造方法
+ *          public Constructor[] getDeclaredConstructors()：获取所有的构造方法(包括私有、受保护、默认、公有)(不包括继承的)
+ *      2).获取单个的方法：
+ *          public Constructor getConstructor(Class... parameterTypes):获取单个的"公有的"构造方法：
+ *          public Constructor getDeclaredConstructor(Class... parameterTypes):获取"某个构造方法"可以是私有的，或受保护、默认、公有
+ *
+ * 调用构造方法：
+ *     Constructor-->newInstance(Object... initargs)
+ */
+public class ConstructorCollector {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        Class clazz = Class.forName("demo.reflect.ReflectTarget");
+
+        //1.获取所有的公有构造方法
+        System.out.println("**********************所有公有构造方法*********************************");
+        Constructor[] conArray = clazz.getConstructors();
+        for(Constructor c : conArray){
+            System.out.println(c);
+        }
+
+        //2.获取所有构造方法
+        System.out.println("************所有的构造方法(包括：私有、受保护、默认、公有)***************");
+        conArray = clazz.getDeclaredConstructors();
+        for(Constructor c : conArray){
+            System.out.println(c);
+        }
+
+        //3.获取单个带参数的公有方法
+        System.out.println("*****************获取公有、有两个参数的构造方法*******************************");
+        Constructor con = clazz.getConstructor(String.class, int.class);
+        System.out.println("con = " + con);
+
+        //4.获取单个私有的构造方法
+        System.out.println("******************获取私有构造方法*******************************");
+        con = clazz.getDeclaredConstructor(int.class);
+        System.out.println("private con = " + con);
+        System.out.println("******************调用私有构造方法创建实例*******************************");
+        //暴力访问（忽略掉访问修饰符）
+        con.setAccessible(true);
+        ReflectTarget reflectTarget = (ReflectTarget) con.newInstance(1);
+    }
+}
--- src/main/java/demo/reflect/ReflectTarget.java
+++ src/main/java/demo/reflect/ReflectTarget.java
@@ -7,6 +7,39 @@ package demo.reflect;
  */
 public class ReflectTarget {
 
+    //---------构造函数-----------
+    //(默认的带参数构造函数)
+    ReflectTarget(String str) {
+        System.out.println("(默认)的构造方法 s = " + str);
+    }
+
+    //无参构造函数
+    public ReflectTarget() {
+        System.out.println("调用了公有的无参构造方法 。。。");
+    }
+
+    //有一个参数的构造函数
+    public ReflectTarget(char name) {
+        System.out.println("调用了带有一个参数的构造方法，参数值为 " + name);
+    }
+
+    //有多个参数的构造函数
+    public ReflectTarget(String name, int index) {
+        System.out.println("调用了带有多个参数的构造方法，参数值为【目标名】： " + name + " 【序号】" + index);
+    }
+    //受保护的构造函数
+    protected ReflectTarget(boolean n){
+        System.out.println("受保护的构造方法 n :" + n);
+    }
+    //私有的构造函数
+    private ReflectTarget(int index){
+        System.out.println("私有的构造方法 序号：" + index);
+    }
+
+
+
+
+
     public static void main(String[] args) throws ClassNotFoundException {
         //第一种方式获取Class对象
         ReflectTarget reflectTarget = new ReflectTarget();
