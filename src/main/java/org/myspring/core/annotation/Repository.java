06050d0772ef5c7ae8cbec91483d6fefecf57261
wujieren <rwj005@outlook.com>
2022-10-28 22:31:18 +0800

    自研IOC容器：自定义注解，用来标记要被IOC容器管理的类。

---
+++ src/main/java/org/myspring/core/annotation/Repository.java
@@ -0,0 +1,16 @@
+package org.myspring.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author rwj
+ * @Date 2022/10/28
+ * @Description
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Repository {
+}
