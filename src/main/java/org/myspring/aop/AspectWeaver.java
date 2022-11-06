772569952e4bbb773f158d63b1db2ace9d1f2b0f
wujieren <rwj005@outlook.com>
2022-11-06 09:32:08 +0800

    自研AOP：3.将横切逻辑织入代理对象，生成代理对象，并注入容器中  —— 代码优化

--- src/main/java/org/myspring/aop/AspectWeaver.java
+++ src/main/java/org/myspring/aop/AspectWeaver.java
@@ -10,7 +10,9 @@ import org.myspring.util.ValidationUtil;
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @Author rwj
@@ -19,10 +21,17 @@ import java.util.Set;
  *      1. 将横切逻辑织入到被代理对象，以生成动态代理对象
  *          1. 创建被代理对象的动态代理实例
  *              1. 获取所有被 @Aspect 标记的切面类
- *              2. 遍历容器中所有 Class（即所有Bean对应的Key），找到该 Class 需要被植入的 切面类集合
- *              3. 按照不同的织入目标（即容器中Class标记的代理Bean）分别按序织入Aspect逻辑
+ *              2. 将切面类按照不同的织入目标进行切分
+ *              3. 按照不同织入目标按序织入Aspect的逻辑
+ *                  1. 获取被代理类的集合
+ *                  遍历
+ *                      1. 创建被代理类的代理对象实例
  *      2. 织入
  *          将动态代理实例添加到容器里，取代未被代理的实例
+ *
+ *
+ * 这种方式比第一种方式要好很多，第一种方式是对容器中所有Bean进行遍历同时判断每个Bean是否需要织入逻辑
+ * 而且通过 Controller.. 等注解去容器中获取被这些注解标记的Bean，大大的减少了对容器中Bean扫描的数量
  */
 public class AspectWeaver {
 
@@ -38,53 +47,52 @@ public class AspectWeaver {
         if (ValidationUtil.isEmpty(aspectSet)) {
             return;
         }
-        // 2.构造AspectInfo
-        List<AspectInfo> aspectInfoList = packAspectInfoList(aspectSet);
-        // 3.遍历容器中所有 Class
-        Set<Class<?>> classSet = beanContainer.getClasses();
-        /**
-         * for循环里这部分代码可以优化
-         *  1. 这里相当于对 classSet 遍历的同时 ， 对 aspectInfoList 进行了 嵌套遍历 时间复杂度 O(n^2)
-         * 优化方式；
-         *  通过对 aspectInfoList 单独进行遍历（不在对 classSet 遍历的for循环中遍历，而是单独提出来在与 classSet for循环 同一层）
-         *  构造一个 Map<targetClass, List<AspectInfo>>，然后再在 对 classSet 遍历的for循环中通过 map.get(targetClass) 获取到对应的 List<AspectInfo>
-         *  这样代码执行效率就提高了，为 O(n)
-         */
-        for (Class<?> targetClass : classSet) {
-            if (targetClass.isAnnotationPresent(Aspect.class)) { //如果被 @Aspect 标记了就跳过，不能对切面类本身织入切面逻辑
-                continue;
+        // 2.将切面类按照不同的织入目标进行切分
+        Map<Class<? extends Annotation>, List<AspectInfo>> categorizedMap = new ConcurrentHashMap<>();
+        for(Class<?> aspectClass : aspectSet) {
+            if(verifyAspect(aspectClass)) {
+                categorizedAspect(categorizedMap, aspectClass);
+            } else {
+                throw new RuntimeException("@Aspect and @Order must be added to the Aspect class, and Aspect class must extend from DefaultAspect");
             }
-            List<AspectInfo> roughMatchedAspectList  = collectRoughMatchedAspectListForSpecificClass(targetClass, aspectInfoList);
-            AspectInterceptor aspectInterceptor = new AspectInterceptor(targetClass, roughMatchedAspectList);
+        }
+        // 3.按照不同织入目标按序织入Aspect的逻辑
+        if(ValidationUtil.isEmpty(categorizedMap)) {
+            return;     //没有任何需要织入的切面类，直接返回
+        }
+        for(Class<? extends Annotation> category : categorizedMap.keySet()) {
+            weaveByCategory(category, categorizedMap.get(category));
+        }
+    }
+
+    /** 这种方式比第一种方式要好很多，第一种方式是对容器中所有Bean进行遍历同时判断每个Bean是否需要织入逻辑 */
+    /** 而且通过 Controller.. 等注解去容器中获取被这些注解标记的Bean，大大的减少了对容器中Bean扫描的数量 */
+    private void weaveByCategory(Class<? extends Annotation> category, List<AspectInfo> aspectInfoList) {
+        // 1. 获取被代理类的集合
+        Set<Class<?>> classSet = beanContainer.getClassesByAnnotation(category);
+        // 2. 遍历被代理类
+        for(Class targetClass : classSet) {
+            // 创建实例
+            AspectInterceptor aspectInterceptor = new AspectInterceptor(targetClass, aspectInfoList);
             Object proxyBean = ProxyCreator.createProxy(targetClass, aspectInterceptor);
+            // 3. 将动态代理对象实例添加到容器中，取代未被代理的实例
             beanContainer.addBean(targetClass, proxyBean);
         }
     }
 
-    private List<AspectInfo> collectRoughMatchedAspectListForSpecificClass(Class<?> targetClass, List<AspectInfo> aspectInfoList) {
-        List<AspectInfo> roughMatchedAspectList = new ArrayList<>();
-        for(AspectInfo aspectInfo : aspectInfoList) {
-            Class<? extends Annotation> value = aspectInfo.getAspect().getClass().getAnnotation(Aspect.class).value();
-            if(targetClass.isAnnotationPresent(value)) {    // 如果目标类的Bean类型（Controller、Component、Service、Repository）和 @Aspect.value() 相等，说明需要被织入切面类逻辑
-                roughMatchedAspectList.add(aspectInfo);
-            }
+    private void categorizedAspect(Map<Class<? extends Annotation>, List<AspectInfo>> categorizedMap, Class<?> aspectClass) {
+        Order orderTag = aspectClass.getAnnotation(Order.class);
+        Aspect aspectTag = aspectClass.getAnnotation(Aspect.class);
+        DefaultAspect aspectObject = (DefaultAspect) beanContainer.getBean(aspectClass);
+        AspectInfo aspectInfo = new AspectInfo(orderTag.value(), aspectObject);
+        if(ValidationUtil.isEmpty(categorizedMap.get(aspectTag.value()))) {
+            List<AspectInfo> aspectInfoList = new ArrayList<>();
+            aspectInfoList.add(aspectInfo);
+            categorizedMap.put(aspectTag.value(), aspectInfoList);
+        } else {
+            List<AspectInfo> aspectInfoList = categorizedMap.get(aspectTag.value());
+            aspectInfoList.add(aspectInfo);
         }
-        return roughMatchedAspectList;
-    }
-
-    private List<AspectInfo> packAspectInfoList(Set<Class<?>> aspectSet) {
-        List<AspectInfo> aspectInfoList = new ArrayList<>();
-        for (Class<?> aspectClass : aspectSet) {
-            if (verifyAspect(aspectClass)) {     // 如果通过验证，就构造为 AspectInfo，并添加到 aspectInfoList
-                Order orderTag = aspectClass.getAnnotation(Order.class);
-                DefaultAspect aspectBean = (DefaultAspect) beanContainer.getBean(aspectClass);
-                AspectInfo aspectInfo = new AspectInfo(orderTag.value(), aspectBean);
-                aspectInfoList.add(aspectInfo);
-            } else {    // 没有通过验证直接报错
-                throw new RuntimeException("@Aspect and @Order must be added to the Aspect class, and Aspect class must extend from DefaultAspect");
-            }
-        }
-        return aspectInfoList;
     }
 
     /**
